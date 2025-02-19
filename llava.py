c713e784e63e7ac07416508aa861d72febf8fdc1
molo <>
2025-02-19 16:19:32 +0100

    Prompt in file & run without printer

--- llava.py
+++ llava.py
@@ -12,15 +12,7 @@ load_dotenv()
 SCREENSHOT_CAM_FILE = Path(str(os.getenv("SCREENSHOT_CAM_FILE")))
 OUTPUT_MSG_FILE = Path(str(os.getenv("OUTPUT_MSG_FILE")))
 SERIAL_PATH = str(os.getenv("SERIAL_PATH"))
-LLAVA_PROMPT = """
-Imagine you we are playing a game and we need you to play an evil persone trying to describe the person in the picture you are provided
-Look at the person whose face is in the green square, imagine a reason why you should arrest this person.
-Fully impersonate your persona from the begining of you answer
-Write like you where writing your deposition of a case 
-Use military alphabet if needed.
-Do not print stage directions.
-Be concise
-"""
+LLAVA_PROMPT = Path("prompts", "prompt.txt").read_text()
 
 
 def ollama_stream(prompt):
@@ -51,15 +43,12 @@ def template(content, printer):
 
 if __name__ == "__main__":
     try:
-        printer = Printer(SERIAL_PATH, debug=False)
+        printer = Printer(SERIAL_PATH, debug=True)
         printer.ser.image("./img/ami.jpg", center=True)
-        # printer.write_datetime()
+        printer.write_datetime()
         with open(OUTPUT_MSG_FILE, "w") as out_file:
             for content in ollama_stream(LLAVA_PROMPT):
-                if content == "\n":
-                    printer.write_divider()
-                else:
-                    printer.text(content)
+                printer.text(content)
                 out_file.write(content)
                 time.sleep(0.1)
         printer.ser.ln()
